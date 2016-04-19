7572370e7df53ff9d855fe70d842bacb364d4cb0
srkg <vindhyachaltiwari01@gmail.com>
2016-04-19 20:19:14 +0530

    update home

---
+++ app/src/main/java/com/example/srkg/pharmafrndz/AppConfig.java
@@ -0,0 +1,12 @@
+package com.example.srkg.pharmafrndz;
+
+/**
+ * Created by srkg on 4/19/2016.
+ */
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://www.pharmafrndz.com/pharmafrndz/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://www.pharmafrndz.com/pharmafrndz/registration.php";
+}
