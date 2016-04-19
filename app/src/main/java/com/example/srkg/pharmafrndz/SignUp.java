b375f72c71b92a31f9d20234ba574f70144eb6b9
srkg <vindhyachaltiwari01@gmail.com>
2016-04-19 19:44:49 +0530

    update home

--- app/src/main/java/com/example/srkg/pharmafrndz/SignUp.java
+++ app/src/main/java/com/example/srkg/pharmafrndz/SignUp.java
@@ -2,55 +2,51 @@ package com.example.srkg.pharmafrndz;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.List;
+public class SignUp extends Activity implements
+        AdapterView.OnItemSelectedListener {
 
-public class SignUp extends Activity implements AdapterView.OnItemSelectedListener {
+    String[] country = { "India", "USA", "China", "Japan", "Other",  };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-        // Spinner element
-        Spinner spinner = (Spinner) findViewById(R.id.spinner1);
+        //Getting the instance of Spinner and applying OnItemSelectedListener on it
+        Spinner spin = (Spinner) findViewById(R.id.spinner1);
+        spin.setOnItemSelectedListener(this);
 
-        // Spinner click listener
-        spinner.setOnItemSelectedListener(this);
+        //Creating the ArrayAdapter instance having the country list
+        ArrayAdapter aa = new ArrayAdapter(this,R.layout.spinner_item,country);
+        aa.setDropDownViewResource(R.layout.spinner_item);
+        //Setting the ArrayAdapter data on the Spinner
+        spin.setAdapter(aa);
+    }
 
-        // Spinner Drop down elements
-        List<String> categories = new ArrayList<String>();
-        categories.add("City");
-        categories.add("Automobile");
-        categories.add("Business Services");
-        categories.add("Computers");
-        categories.add("Education");
-        categories.add("Personal");
-        categories.add("Travel");
-        // Creating adapter for spinner
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
 
-        // Drop down layout style - list view with radio button
-        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        // attaching data adapter to spinner
-        spinner.setAdapter(dataAdapter);
+    //Performing action onItemSelected and onNothing selected
+    @Override
+    public void onItemSelected(AdapterView<?> arg0, View arg1, int position,long id) {
+        Toast.makeText(getApplicationContext(),country[position] ,Toast.LENGTH_LONG).show();
     }
 
     @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        // On selecting a spinner item
-        String item = parent.getItemAtPosition(position).toString();
-
-        // Showing selected spinner item
-        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
-    }
     public void onNothingSelected(AdapterView<?> arg0) {
         // TODO Auto-generated method stub
+
     }
+
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+            // Inflate the menu; this adds items to the action bar if it is present.
+            getMenuInflater().inflate(R.menu.menu, menu);
+            return true;
+
+        }
 }
